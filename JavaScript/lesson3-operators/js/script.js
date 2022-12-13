ac9d73e8ba10ff4cc024ac460d1d8483b2219ed2
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-12-13 20:54:22 +0400

    js

---
+++ JavaScript/lesson3-operators/js/script.js
@@ -0,0 +1,57 @@
+// debugger
+// consaasdasdle.log("Hello");
+
+
+// Assignment operators
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+// **=
+
+// var infoOne = 5;
+// infoOne**=10;// infoOne =  infoOne + 10
+// console.log(infoOne);
+
+
+// Arithmetic data operators  
+
+// +
+// -
+// /
+// *
+// %
+// **
+// ++
+// --
+
+// var info = 5;
+// info--;
+// console.log(info);
+
+
+// Comparison operators
+
+// ==
+// !=
+// ===
+// !==
+// <
+// <=
+// >
+// >=
+
+// var infoOne = "A";
+// var infoTwo = "a";
+
+// if(infoOne < infoTwo){
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+
+
+
