5782084121c98275a98477bfb66c57cdc42b7c55
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-12-17 21:03:51 +0400

    if

--- JavaScript/lesson3-operators/js/script.js
+++ JavaScript/lesson3-operators/js/script.js
@@ -52,6 +52,84 @@
 //     console.log(false);
 // }
 
+// Logical operators
+// && - AND
+// || - OR
+// !  - NOT
+
+// if (false || false || !true || false) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
 
 
+// string operators
+// =
+// +=
+
+
+// var a = "JavaScript";
+// a+= " Hello"
+
+// console.log(a);
+
+
+
+ // --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,            (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+// var a = 5, b = 10;
+// console.log(a,b); 
+// \            (tərs slash)(Qaçındırma operatorudur)
+// var info = "Hasan\"s Book";
+// console.log(info);
+
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+var info = 5;
+// console.log(typeof info);
+// console.log(typeof(info));
+// ternary 
+// ?:           (sual üst-üstə qoşa nöqtə)((şərt) ? ifadə1 : ifadə2)(ternary)
+// 5>10 ? console.log('5 is little') : console.log(false);
+// 5<10 ? console.log('duzdu'):console.log('sehvdir');
+// (5 >= 10) ? document.write("Hi") : document.write("Bye");
+// şərt            ? true                 : false
+
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+// var a = 5;
+// console.log(void(a));
+// delete       (objectin dəyərini silmək üçündür)
+// var a = {
+//     name:"Hello"
+// }
+// delete a.name
+// console.log(a.name);
+// new          (yeni dəyər yaratməq üçündür)
+// class a{
+//     constructor(name){
+//         this.ad = name;
+//     }
+//     sayHi(){
+//         return `Hello ${this.ad}`
+//     }
+// }
+
+// var b = new a("Rufat");
+// console.log(b.sayHi());
+// this         (mövcud dəyəri işarələmək üçündür)
+// in           (dəyişkənin(object) daxilində dəyərin olub olmamasını yoxlamaq üçündür)
+
+// instanceof   (dəyişkənin(objectin xüsusiyyətinin) içində xüsusiyyət olub olmamasını yoxlamaq üçündür)
+
+
+
+
+
+    
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
 
