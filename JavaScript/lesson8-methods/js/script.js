51c27e02103a345e4803c1c6c72cafdbac426089
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-01-10 20:58:12 +0400

    DOM part 1

--- JavaScript/lesson8-methods/js/script.js
+++ JavaScript/lesson8-methods/js/script.js
@@ -1,4 +1,4 @@
-const info = 5.7;
+// const info = 5.7;
 // console.log((Math.random()*100).toFixed);
 // console.log((Math.random()*100).toExponential);
 // console.log((Math.random()*100));
@@ -7,23 +7,66 @@ const info = 5.7;
 
 
 
-const allData = [];
+// const allData = [];
 
-function addData(newData){
-    allData.push(newData);
-}
+// function addData(newData){
+//     allData.push(newData);
+// }
 
-function delData(){
-    allData.pop();
-}
+// function delData(){
+//     allData.pop();
+// }
+
+// addData('new value1')
+// addData('new value2')
+// addData('new value3 ')
+// // delData()
+
+// console.log(allData);
+
+// const info = "                              JavaScript           Lessons         ";
+// const newInfo = info.trim();
+// console.log(newInfo);
+
+// const info = "Hello";
+// console.log(eval("info"));
+// console.log(info.length);
+
+
+// const infoText = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, aut suscipit quidem excepturi quibusdam, quasi a maiores magnam nobis unde inventore officiis at cum, aperiam officia. Beatae a mollitia vero.";
+
+// console.log(infoText.substring(6,100)+"...");
+
+// const info = 'Hello';
+
+// console.log(info.repeat(100));
+
+
+// const infoData = "JavaScript Lesson";
+// https://www.w3schools.com/jsref/jsref_regexp_g.asp
+// const total = infoData.match(/s/gi);
+// console.log(total);
 
 
 
 
-addData('new value1')
-addData('new value2')
-addData('new value3 ')
-// delData()
+import { product } from "./data.js";
 
-console.log(allData);
+// product.map((fd,i)=>{
+//     return console.log(i+"-------"+fd.title);
+// })
+// product.forEach((fd,i)=>{
+//     return console.log(i+"-------"+fd.title);
+// })
+// product.filter((item)=>{
+//     return console.log(item.category ==="women's clothing");
+// })
 
+
+
+
+// const ages = [32, 33, 16, 40];
+
+// ages.filter(function(age) {
+//     return console.log(age >= 18);;
+//   });
