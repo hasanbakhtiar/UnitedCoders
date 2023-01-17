3b3e6f819b6dfa0dc321aaebdc1b9d2526de6280
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-01-17 20:28:44 +0400

    Dom part 2

---
+++ JavaScript/lesson10-DOMpart2/js/script.js
@@ -0,0 +1,32 @@
+// const text = document.querySelector('h1');
+// const btn = document.querySelector('button');
+
+
+// btn.addEventListener('click', ()=>{
+//     text.style.color = 'red';
+//     text.style.fontSize = '50px';
+//     text.style.transition = '1s';
+// });
+
+
+
+// const text = document.querySelector('h1');
+// const btn = document.querySelector('button');
+
+
+// btn.addEventListener('click',()=>{
+//     text.attributes[0].value = 'myred'
+// })
+
+
+
+// const btn = document.querySelector('button');
+// const menu  = document.querySelector('menu');
+
+// btn.addEventListener('click',()=>{
+//   if (menu.attributes[0].value === 'show') {
+//     menu.attributes[0].value = 'hide'
+//   }else{
+//     menu.attributes[0].value = 'show'
+//   }
+// })
