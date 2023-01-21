4b7d7239f01d67ba29619df00873ece83f6282d0
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-01-21 20:24:05 +0400

    localStorage

---
+++ JavaScript/lesson12-DOMpart4-localStorage/localStorageExample/js/script.js
@@ -0,0 +1,10 @@
+// localStorage.setItem
+// localStorage.getItem
+// localStorage.removeItem
+// localStorage.clear
+// localStorage.setItem('value','new value')
+// document.querySelector('h1').innerHTML = localStorage.getItem('text')
+// localStorage.removeItem('text');
+// localStorage.clear()
+
+
