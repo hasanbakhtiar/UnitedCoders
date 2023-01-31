9f976c861b126bf42a4308766ad071a9cf8b1ead
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-01-31 20:53:01 +0400

    api with ts

--- JavaScript/TypeScript/lesson1-introduction/ts/app.ts
+++ JavaScript/TypeScript/lesson1-introduction/ts/app.ts
@@ -3,38 +3,57 @@ var input:any = document.querySelector<HTMLInputElement>('input');
 var text:string = "Hello";
 // ol.innerHTML = text;
 
-type productType={
-    title:string,
-    price:number
+// type productType={
+//     title:string,
+//     price:number
+// }
+
+
+// var product:productType[] = [
+//     {
+//         title:'pen',
+//         price:20
+//     },
+//     {
+//         title:'book',
+//         price:200
+//     },
+//     {
+//         title:'ruler',
+//         price:50
+//     }
+// ]
+
+
+// var fetchProduct = ():void=>{
+//     let li:string="";
+//     product.map((item:any,i:number):void=>{
+//         li+=`<li>${item.title}</li>`
+//     })
+//     ol.innerHTML = li;
+
+// }
+// fetchProduct();
+
+interface postType{
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+
 }
 
 
-var product:productType[] = [
-    {
-        title:'pen',
-        price:20
-    },
-    {
-        title:'book',
-        price:200
-    },
-    {
-        title:'ruler',
-        price:50
-    }
-]
-
-
-var fetchProduct = ():void=>{
-    let li:string="";
-    product.map((item:any,i:number):void=>{
-        li+=`<li>${item.title}</li>`
+var fetchApi=async()=>{
+    let comingData:any = await fetch('https://jsonplaceholder.typicode.com/posts')
+    let mydata:postType[] = await comingData.json();
+    let li:string = "";
+    mydata.map((item:any)=>{
+        li+= `<li>${item.title}</li>`
     })
     ol.innerHTML = li;
-
 }
-fetchProduct();
-
+fetchApi();
 
 
 
