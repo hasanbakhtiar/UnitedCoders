e8f4fb45255567b7ac6e0f2c8a705e0480a5eef8
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-02-07 21:00:13 +0400

    ajax setInterval

--- JavaScript/VanillaJs/lesson19-BOMandAjaxPrototype/js/script.js
+++ JavaScript/VanillaJs/lesson19-BOMandAjaxPrototype/js/script.js
@@ -24,15 +24,48 @@
 
 
 "use strict"
-const loadDoc=()=> {
-    const xhttp = new XMLHttpRequest();
+// const loadDoc=()=> {
+//     const xhttp = new XMLHttpRequest();
 
-    xhttp.onload = function() {
-        document.querySelector('p').innerHTML = this.responseText;
-      }
-      xhttp.open("GET", "data/info.txt");
-      xhttp.send();
+//     xhttp.onload = function() {
+//         document.querySelector('p').innerHTML = this.responseText;
+//       }
+//       xhttp.open("GET", "data/info.txt");
+//       xhttp.send();
     
-  }
+//   }
 
-document.querySelector('button').onclick = loadDoc;
+// document.querySelector('button').onclick = loadDoc;
+
+// setTimeout(()=>{},time)
+
+// 1000ms  = 1s 
+
+// const info = ()=>{
+//   console.log('setTimeOut running');
+// }
+// setTimeout(info,1000);
+
+// const info = ()=>{
+//   console.log('setInterval running');
+// }
+// const interval =  setInterval(info,1000);
+
+// document.querySelector('button').onclick=()=>{
+//     clearInterval(interval)
+// }
+// setInterval(()=>{},time)
+
+
+const preloader = document.querySelector('.preloader');
+const wrapper = document.querySelector('.wrapper');
+wrapper.style.display = 'none'
+
+setTimeout(()=>{
+  wrapper.style.display = 'block';
+  preloader.style.display = 'none'
+},2000);
+
+setInterval(()=>{
+  document.querySelector('.time').innerHTML = new Date();
+},1000);
