14d6112f0f825c0e2f053b8489b38f77053b740f
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-02-11 21:01:31 +0400

    introduction

---
+++ JavaScript/ReactJS/lesson1-introduction/public/scripts/app.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var list = /*#__PURE__*/React.createElement("ul", null, /*#__PURE__*/React.createElement("li", null, "list item"), /*#__PURE__*/React.createElement("li", null, "list item"), /*#__PURE__*/React.createElement("li", null, "list item"), /*#__PURE__*/React.createElement("li", null, "list item"), /*#__PURE__*/React.createElement("li", null, "list item"));
+var temp = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", null, "Hello React"));
+ReactDOM.render(temp, document.querySelector('#root'));
---
+++ JavaScript/ReactJS/lesson1-introduction/src/index.js
@@ -0,0 +1,20 @@
+const list = (
+    <ul>
+        <li>list item</li>
+        <li>list item</li>
+        <li>list item</li>
+        <li>list item</li>
+        <li>list item</li>
+    </ul>
+)
+
+
+const temp = (
+    <>
+        <h1>Hello React</h1>
+
+        
+    </>
+)
+
+ReactDOM.render(temp,document.querySelector('#root'));
