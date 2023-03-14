e9635c68ac69f047b864f4d21409e438a78f878c
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-03-14 18:49:07 +0400

    hook

--- JavaScript/ReactJS/lesson13-otherhook/src/components/AppUseImperativeHandleElpachoErnandes.jsx
+++ JavaScript/ReactJS/lesson13-otherhook/src/components/AppUseImperativeHandleElpachoErnandes.jsx
@@ -2,16 +2,28 @@ import React,{useImperativeHandle, forwardRef, useRef, useState} from 'react'
 
 const MyInput =forwardRef((props,ref)=>{
   const [fontSize,setFontSize] = useState(14);
+  const [styleName,setStyleName] = useState('danger');
+  const [value,setValue] = useState('');
 useImperativeHandle(ref,()=>({
   hi:sayHi,
   incFont:()=>{setFontSize(fontSize+2)},
-  decFont:()=>{setFontSize(fontSize-2)}
+  decFont:()=>{setFontSize(fontSize-2)},
+  changeColor:newColor
+  
 }))
   const sayHi=()=>{
     alert("Hello")
   }
+
+  const newColor =()=>{
+    setValue(styleName);
+  }
   return(
-    <input style={{fontSize}} type="text" />
+   <>
+    <h1 className={`text-${value}`}>AppUseImperativeHandleElpachoErnandes</h1>
+    <input onChange={e=>{setStyleName(e.target.value)}} style={{fontSize}} type="text" />
+   </>
+   
   )
 })
 
@@ -19,12 +31,12 @@ useImperativeHandle(ref,()=>({
 const AppUseImperativeHandleElpachoErnandes = () => {
   const inputRef = useRef();
   const handleClick =()=>{
-    inputRef.current.hi()
+    // inputRef.current.hi()
+    inputRef.current.changeColor();
   }
   
   return (
     <div>
-      <h1>AppUseImperativeHandleElpachoErnandes</h1>
       <MyInput ref={inputRef}/><button onClick={handleClick}>add</button>
       <button onClick={()=>{inputRef.current.decFont()}}>-</button>
       <button onClick={()=>{inputRef.current.incFont()}}>+</button>
