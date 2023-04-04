4d409d24b14381859f054bb1cd58906f67b6b069
hasanbakhtiar <hasan.doctype@gmail.com>
2023-04-05 00:54:54 +0400

    redux blog app part 2

--- JavaScript/ReduxJS/lesson3-blogapp-part2/src/pages/BlogDetails.jsx
+++ JavaScript/ReduxJS/lesson3-blogapp-part2/src/pages/BlogDetails.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import { connect } from 'react-redux';
+import { useParams } from 'react-router-dom'
 
-const BlogDetails = () => {
+const BlogDetails = ({pvalue}) => {
+  const {id} = useParams();
+  const detailsItem = pvalue.find(p=>p.id ==id);
   return (
    
 <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
   <div className="col-10 col-sm-8 col-lg-6">
-    <img src="bootstrap-themes.png" className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" loading="lazy" width={700} height={500} />
+    <img src={detailsItem.photo} className="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" loading="lazy" width={700} height={500} />
   </div>
   <div className="col-lg-6">
-    <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Responsive left-aligned hero with image</h1>
-    <p className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+    <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">{detailsItem.title}</h1>
+    <p className="lead">{detailsItem.desc}</p>
     <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-      <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
-      <button type="button" className="btn btn-outline-secondary btn-lg px-4">Default</button>
     </div>
   </div>
 </div>
@@ -21,4 +23,10 @@ const BlogDetails = () => {
   )
 }
 
-export default BlogDetails
+const mapStateToProps =(state)=>{
+  return {
+    pvalue:state
+  }
+}
+
+export default connect(mapStateToProps)(BlogDetails)
