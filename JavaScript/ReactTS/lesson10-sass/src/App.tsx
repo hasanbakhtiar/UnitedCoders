19e7baf8f71eb0fc21396210504a1a9588b4ccaa
Hasan Bakhtiar <hasan.doctype@gmail.com>
2023-05-13 14:31:58 +0400

    sass

---
+++ JavaScript/ReactTS/lesson10-sass/src/App.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+        <ul>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+        </ul>
+
+
+        <h1 className='icon-40px'>Hello</h1>
+        <h1 className='icon-50px'>Hello</h1>
+        <h1 className='icon-80px'>Hello</h1>
+
+<p>x<sup>2</sup></p>
+    </div>
+  )
+}
+
+export default App
